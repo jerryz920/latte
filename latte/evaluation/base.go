83fd95d6f2d85e01fee7e21ec2e21ecbc9d81628
Yan Zhai <zhaiyan920@gmail.com>
2018-05-08 05:04:38 -0500

    fix the reuse problem

---
+++ latte/evaluation/base.go
@@ -0,0 +1,65 @@
+package latte
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+)
+
+type MetadataRequest struct {
+	Principal   string   `json:"principal"`
+	OtherValues []string `json:"otherValues"`
+}
+
+type MetadataClient struct {
+	Host   string
+	Client http.Client
+}
+
+func (m *MetadataClient) Request(cmd string, principal string, otherValues ...string) error {
+	tx := time.Now()
+	buf := bytes.NewBuffer(nil)
+	data := MetadataRequest{
+		Principal:   principal,
+		OtherValues: otherValues,
+	}
+	encoder := json.NewEncoder(buf)
+	if err := encoder.Encode(&data); err != nil {
+		logrus.Error("error in json encoding: ", err)
+		return err
+	}
+	url := fmt.Sprintf("%s%s", m.Host, cmd)
+	resp, err := m.Client.Post(url, "application/json", buf)
+	if err != nil {
+		logrus.Error("error in requesting:", err)
+		return err
+	}
+
+	msg, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		logrus.Error("error in readall")
+		return err
+	}
+	ts := time.Now().Sub(tx).Seconds()
+	logrus.WithField("name", cmd).WithField("speaker", principal).
+		WithField("time", ts).Info(string(msg))
+	return nil
+}
+
+func NewClient(addr string) *MetadataClient {
+	return &MetadataClient{
+		Host: addr,
+		Client: http.Client{
+			Transport: &http.Transport{
+				MaxIdleConnsPerHost: 128,
+			},
+			Timeout: time.Second * 10,
+		},
+	}
+}
