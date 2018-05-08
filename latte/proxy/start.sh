3cfb18cf68ab97c909f504002b73101cd8b4eb5c
Yan Zhai <zhaiyan920@gmail.com>
2018-05-07 21:22:13 -0500

    utility quick commit

--- latte/proxy/start.sh
+++ latte/proxy/start.sh
@@ -5,6 +5,7 @@ for n in `seq 1 $num`; do
   safeport=$((7776+n)) 
   listenport=$((19850+n))
   docker run -dt --rm -p $safeport:7777 -e RIAK_IP=10.10.1.4 --name safe$n safe
-  ./proxy --addr 10.10.1.3:8087 --addr 10.10.1.4:8087 --addr 10.10.1.5:8087 --safe localhost:$safeport --listen 0.0.0.0:$listenport --debug 2>&1 | tee perflog-$n
+  ./proxy --addr 10.10.1.3:8087 --addr 10.10.1.4:8087 --addr 10.10.1.5:8087 --safe localhost:$safeport --listen 0.0.0.0:$listenport --debug 2>&1 | tee perflog-$n&
   # start safe container
 done
+
