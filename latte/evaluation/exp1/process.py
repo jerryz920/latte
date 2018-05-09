73ab3938b0291e4db4a2d8c3bc25f7fe8d140d84
Yan Zhai <zhaiyan920@gmail.com>
2018-05-08 20:32:03 -0500

    utility quick commit

--- latte/evaluation/exp1/process.py
+++ latte/evaluation/exp1/process.py
@@ -1,10 +1,29 @@
 import json
 
 import sys
+if len(sys.argv) >= 2:
+    with open(sys.argv[1], "r") as f:
+        for l in f:
+            try:
+                val=json.loads(l)
+            except:
+                continue
+            if not "time" in val:
+                continue
+            if val["msg"] == "finish":
+                print("%f" % val["time"])
+            else:
+                print("%s %f" % (val["name"], val["time"]))
 
-with open(sys.argv[1], "r") as f:
-    for l in f:
-        val=json.loads(l)
+else:
+    for line in sys.stdin:
+        try:
+            val=json.loads(line)
+        except:
+            continue
+        if not "time" in val:
+            continue
+
         if val["msg"] == "finish":
             print("%f" % val["time"])
         else:
