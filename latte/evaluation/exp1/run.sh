b6b60db2de2d13a9096eec3a3309cc7fa0d11fcd
Yan Zhai <zhaiyan920@gmail.com>
2018-05-10 03:55:07 -0500

    utility quick commit

--- latte/evaluation/exp1/run.sh
+++ latte/evaluation/exp1/run.sh
@@ -34,16 +34,16 @@ config() {
 
 }
 
-export myid=80
+export myid=74
 run() {
-  #echo "starting $* exps"
-  #echo "restarting riak"
-  #cd $riakdir
-  #bash all-restart.sh
-  #sleep 5
-  #echo "starting metadata service"
-  #cd $workdir; bash stop.sh 4; bash start.sh 4
-  #sleep 20
+  echo "starting $* exps"
+  echo "restarting riak"
+  cd $riakdir
+  bash all-restart.sh
+  sleep 5
+  echo "starting metadata service"
+  cd $workdir; bash stop.sh 4; bash start.sh 4
+  sleep 20
   args=`config $*`
   cd $workdir
   echo "running ./exp1 $args"
@@ -93,22 +93,22 @@ run() {
 # vary nvm
 #for n in 128 256 512 1024; do
 #  for j in 1 2 3 4 5; do
-#    run 8 $n 3 1 $j 
-#    run 8 $n 3 0 $j 
+#    run 128 $n 3 1 $j 
+#    run 128 $n 3 0 $j 
 #  done
 #done
 
 # vary level
 #for n in 1 2; do
 #  for j in 1 2 3 4 5; do
-#    run 8 1024 $n 1 $j 
-#    run 8 1024 $n 0 $j 
+#    run 128 1024 $n 1 $j 
+#    run 128 1024 $n 0 $j 
 #  done
 #done
 
 # vary thread
 ulimit -n 
-for n in 128; do
+for n in 256; do
   for j in 1 2 3 4 5; do
     run $n 1024 3 1 $j 
     run $n 1024 3 0 $j 
